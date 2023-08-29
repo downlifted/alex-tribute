3fa05b4a875fc125fb93bb30025382ea76f36f49
downlifted <38848227+downlifted@users.noreply.github.com>
2023-08-29 08:47:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,26 @@
-const nftContainer = document.querySelector('.nft-container');
-const memoryContainer = document.querySelector('.memory-container');
 const memoryForm = document.getElementById('memory-form');
 
-// Example NFT data (you can replace this with actual data)
-const nfts = [
-    { image: 'nft1.jpg', description: 'NFT 1 description' },
-    { image: 'nft2.jpg', description: 'NFT 2 description' },
-    // Add more NFTs
-];
-
-// Display NFTs in the gallery
-nfts.forEach(nft => {
-    const nftItem = document.createElement('div');
-    nftItem.classList.add('nft-item');
-    nftItem.innerHTML = `
-        <img src="${nft.image}" alt="NFT Image">
-        <p>${nft.description}</p>
-    `;
-    nftContainer.appendChild(nftItem);
-});
-
-// Handle memory submission
-memoryForm.addEventListener('submit', event => {
+memoryForm.addEventListener('submit', async event => {
     event.preventDefault();
     const name = document.getElementById('name').value;
     const memory = document.getElementById('memory').value;
-    const memoryItem = document.createElement('div');
-    memoryItem.classList.add('memory-item');
-    memoryItem.innerHTML = `
-        <p><strong>${name}</strong> said:</p>
-        <p>${memory}</p>
-    `;
-    memoryContainer.appendChild(memoryItem);
-    memoryForm.reset();
+
+    if (!name || !memory) {
+        return;
+    }
+
+    const response = await fetch('/api/submitMemory', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name, memory }),
+    });
+
+    if (response.ok) {
+        // Refresh the page to show the updated memories
+        window.location.reload();
+    } else {
+        console.error('Failed to submit memory');
+    }
 });
